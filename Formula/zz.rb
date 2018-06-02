3fb3a536486cdab56edc2e71c67217c57a5dbb5b
Kaleo Cheng <kaleocheng@gmail.com>
2018-06-02 17:16:27 +0800

    Add version for zz

--- Formula/zz.rb
+++ Formula/zz.rb
@@ -3,6 +3,7 @@ class Zz < Formula
     url "https://github.com/kaleocheng/zz.git"
     sha256 "ed6fc6e2f60aa6167b349ad300e959d11735e18b45e6de6e6bc461b0bda39358"
     head "https://github.com/kaleocheng/zz.git"
+    version "0.0.1"
   
     depends_on "dep" => :build
     depends_on "go" => :build
