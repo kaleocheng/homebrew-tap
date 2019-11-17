68033806ca54d9607d2ec38358e76d9a30db4d59
Kaleo <kaleocheng@gmail.com>
2019-11-17 16:48:39 +0800

    Add lugwig

---
+++ Casks/ludwig.rb
@@ -0,0 +1,10 @@
+cask 'ludwig' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://desktop.ludwig.guru/download/1.1.1/osx_64'
+  name 'Ludwig'
+  homepage 'https://ludwig.guru'
+
+  app 'Ludwig.app'
+end
