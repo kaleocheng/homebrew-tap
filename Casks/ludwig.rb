c69db6290e002f5c3c0fa2d872570c3c71c2dc21
Kaleo <kaleocheng@gmail.com>
2019-11-17 17:05:26 +0800

    Use autoupdate for ludwig

--- Casks/ludwig.rb
+++ Casks/ludwig.rb
@@ -1,6 +1,7 @@
 cask 'ludwig' do
   version '1.1.1'
   sha256 'fb6cb194a0fdfc7b2316f199a57c3e8476293b61dfc697730b9eb700ef2146d8'
+  auto_updates true
 
   url 'https://desktop.ludwig.guru/download/1.1.1/osx_64'
   name 'Ludwig'
